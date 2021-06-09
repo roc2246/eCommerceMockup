8f948f6e9e2fd757ea59d4fb8d50611c65661d4a
Riley <riley.childs@yahoo.com>
2021-06-09 10:26:56 -0400

    UI

---
+++ dropdownScript.js
@@ -0,0 +1,18 @@
+// Hides Child Elements
+function hideSubCats(){
+	var subCats = document.getElementsByTagName("UL");
+	for(var i = 1; i<=3; i++){
+		subCats[i].style.display = "none";
+	}
+}
+
+// Reveals/Hides Child Elements
+function revealCat(ulNo) {
+		document.getElementsByTagName("UL")[ulNo].style.display = "block";
+
+}
+
+function hideCat(ulNo) {
+		document.getElementsByTagName("UL")[ulNo].style.display = "none";
+
+}
--- index.php
+++ index.php
@@ -4,11 +4,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+   
     <title>Document</title>
 </head>
-<body>
-  <p>TEST !!11!!</p>
-  <p>sewrdgwsedg</p>
-  <p>sdgsdg</p>
+<body onload="hideSubCats()">
+<header>
+<h1>The Condo</h1>
+<h4>Second-Rate Bardshop</h4>
+</header>
+<navigation>
+<ul id="selectGear">
+		<li class="category" onmouseover="revealCat(1)" onmouseleave="hideCat(1)">Boards
+			<ul>
+				<li><a href="nothing.html">Ride Society</a></li>
+				<li><a href="nothing.html">Burton Fish</a></li>
+				<li><a href="nothing.html">Rome Agent</a></li>
+			</ul>
+		</li> 
+
+		<li class="category" onmouseover="revealCat(2)" onmouseleave="hideCat(2)">Boots
+			<ul>
+				<li><a href="nothing.html">Vans Infusion</a></li>
+				<li><a href="nothing.html">Burton Ruler</a></li>
+				<li><a href="nothing.html">DC Allegance</a></li>
+			</ul> 
+		</li>
+
+		<li class="category" onmouseover="revealCat(3)" onmouseleave="hideCat(3)">Bindings
+			<ul>
+				<li><a href="nothing.html">Union Flite</a></li>
+				<li><a href="nothing.html">Union Contact</a></li>
+				<li><a href="nothing.html">Burton Mission</a></li>
+			</ul> 
+		</li>
+
+</ul>
+</navigation>
+
+
+<div class="main content">
+
+</div>
+
+<script src="dropDownScript.js"></script>
+
+
 </body>
 </html>
