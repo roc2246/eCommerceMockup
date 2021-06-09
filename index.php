e16e7008cfafe3164b7199c61847526bae3ec397
Riley <riley.childs@yahoo.com>
2021-06-09 18:14:15 -0400

    setting up Cart

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php 
+
+if(isset($_POST['submit'])) {
+   
+$username = $_POST['username'];
+$password = $_POST['password'];
+    
+    
+$connection = mysqli_connect('localhost', 'root', '', 'loginapp');    
+    
+    if($connection) {
+    
+    echo "We are connected";
+    
+    } else {
+    
+    die("Database connection failed");
+    
+    }
+
+}
+
+function readRows() {
+    global $connection;
+    $query = "SELECT * FROM users";
+    $result = mysqli_query($connection, $query);
+    if(!$result) {
+        die('Query FAILED' . mysqli_error($connection));
+    }
+        
+while($row = mysqli_fetch_assoc($result)) {
+        
+        print_r($row);
+    }  
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,6 +55,7 @@
 
 
 <div class="main-content">
+
 
 
 <div id="login">
