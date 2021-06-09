ed729e7958594326a6cff7c2488219091b8feb5f
Riley <riley.childs@yahoo.com>
2021-06-09 11:09:57 -0400

    UI

--- index.php
+++ index.php
@@ -13,41 +13,10 @@
 <h1>The Condo</h1>
 <h4>Second-Rate Bardshop</h4>
 </header>
-<navigation>
-<ul id="selectGear">
-		<li class="category" onmouseover="revealCat(1)" onmouseleave="hideCat(1)">Boards
-			<ul>
-				<li><a href="nothing.html">Ride Society</a></li>
-				<li><a href="nothing.html">Burton Fish</a></li>
-				<li><a href="nothing.html">Rome Agent</a></li>
-			</ul>
-		</li> 
-
-		<li class="category" onmouseover="revealCat(2)" onmouseleave="hideCat(2)">Boots
-			<ul>
-				<li><a href="nothing.html">Vans Infusion</a></li>
-				<li><a href="nothing.html">Burton Ruler</a></li>
-				<li><a href="nothing.html">DC Allegance</a></li>
-			</ul> 
-		</li>
-
-		<li class="category" onmouseover="revealCat(3)" onmouseleave="hideCat(3)">Bindings
-			<ul>
-				<li><a href="nothing.html">Union Flite</a></li>
-				<li><a href="nothing.html">Union Contact</a></li>
-				<li><a href="nothing.html">Burton Mission</a></li>
-			</ul> 
-		</li>
-
-</ul>
-</navigation>
+<?php include 'include/navigaiton.php' ?>
 
 
-<<<<<<< HEAD
 <div class="main-content">
-=======
-<div class="main content">
->>>>>>> 8f948f6e9e2fd757ea59d4fb8d50611c65661d4a
 
 </div>
 
