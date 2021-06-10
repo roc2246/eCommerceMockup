282064fa836aab7dcbd151e175253fa3da6b04b8
Riley <riley.childs@yahoo.com>
2021-06-10 14:20:36 -0400

    Upload Inventory

---
+++ UploadProduct.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="brand">
+        <input type="text" name="product">
+        <input type="text" name="price">
+        <input type="file" name="image">
+<input type="submit">
+    <!--Use php to track amount of an item in inventory
+    https://www.w3schools.com/php/php_mysql_update.asp
+    
+    -->
+    </form>
+</body>
+</html>
