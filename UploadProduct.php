a3a2faa65ca3c8ddfedb5f29b602628aeacc1b8c
Riley <riley.childs@yahoo.com>
2021-06-12 08:40:58 -0400

    Update Inventory

--- UploadProduct.php
+++ UploadProduct.php
@@ -1,4 +1,50 @@
 <?php include 'include/connect.php' ?>
+
+<!--Reformat Later-->
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+//https://www.w3schools.com/php/php_file_upload.asp
+if(isset($_POST["submit"])) {
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+      } else {
+        echo "Sorry, there was an error uploading your file.";
+      }
+}
+?>
+
+<?php 
+if(isset($_POST['submit'])) {
+    global $connection;
+        
+    $brand = $_POST['brand'];
+    $model = $_POST['model'];
+    $size = $_POST['size'];
+    $price = $_POST['price'];
+    $image = $_FILES["image"]["name"];
+
+
+        
+        $query = "INSERT INTO inventory(brand,model,price,size, image) VALUES ('$brand', '$model','$price', '$size', '$image')";
+        
+       $result = mysqli_query($connection, $query);
+        if(!$result) {
+            die('Query FAILED' . mysqli_error($connection));
+        
+        } else {
+        
+        echo "Record Create<br><br>"; 
+        
+        }
+    }
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,50 +54,20 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="uploadProduct.php" method="post" enctype="multipart/form-data">
         <input type="text" name="brand">
-        <input type="text" name="product">
+        <input type="text" name="model">
         <input type="text" name="price">
+        <input type="text" name="size">
         <input type="file" name="image">
-<input type="submit">
+<input type="submit" name="submit">
     <!--Use php to track amount of an item in inventory
     https://www.w3schools.com/php/php_mysql_update.asp
     
     -->
     </form>
 
-<!--Reformat Later-->
-<?php 
-if(isset($_POST['submit'])) {
-    global $connection;
-        
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-        
-    $username = mysqli_real_escape_string($connection, $username );   
-    $password = mysqli_real_escape_string($connection, $password );
-       
-    
-    $hashFormat = "$2y$10$"; 
-    $salt = "iusesomecrazystrings22";
-    $hashF_and_salt = $hashFormat . $salt;
-    $password = crypt($password,$hashF_and_salt);   
-        
-        $query = "INSERT INTO users(username,password) ";
-        $query .= "VALUES ('$username', '$password')";
-        
-       $result = mysqli_query($connection, $query);
-        if(!$result) {
-            die('Query FAILED' . mysqli_error());
-        
-        } else {
-        
-        echo "Record Create"; 
-        
-        }
 
-
-?>
 
 
 </body>
