e80626a73cab0a39a0e0729b5174ed6dd362aff4
Riley <riley.childs@yahoo.com>
2021-07-15 10:36:48 -0400

    Refactor AJAX, Another Way Checkout

--- index.php
+++ index.php
@@ -71,9 +71,15 @@ function removeItem(itemNo){
 
 </div>
 <!---------------------------------------------------->
-<?php //updateBasket();?>
+
+<?php //updateBasket();?><!--For Potentail AJAX-->
 <script>
-document.getElementById("count").innerHTML = 0;
+document.getElementById("count").innerHTML = 0; //counts number of items in cart
+
+function checkoutItems(){
+document.getElementById("checkout-purchases").appendChild(cartItem);//For checkout window
+
+}
 
 function toCart(i){
     c++;
@@ -105,86 +111,7 @@ document.getElementById("count").innerHTML = cart.childElementCount;
 }
 
 
-/*-------------------Chekcout Window---------------------------------------------*/
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-/*-------------------Data inside checkout window--------------------------------*/
-//Tracks the request
-var xmlhttp;
-
-//Checks if XMLHttpRequest is compatible with the browser
-function compatibility() {
-  if (window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-  } else {
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-}
-
-//Loads items in basket
-function loadPBasket(url) {
-  compatibility();
-  xmlhttp.onreadystatechange = function() {
-    //Keeps the 'Failed Request' message from displaying during the request
-    if (xmlhttp.readyState !=4){
-        return;
-    }
-    if (xmlhttp.status==200 && xmlhttp.readyState ==4) {
-        setPS();
-    } else {
-        alert("Failed Request: " + xmlhttp.statusText);
-    }
-  };
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
-}
-
-//Sets basket items to checkout window
-function setPS() {
-  document.getElementById("checkout-purchases").innerHTML = " ";
-  var i;
-  var xmlDoc = xmlhttp.responseXML;
-  var x = xmlDoc.getElementsByTagName("item");
-  for (i = 0; i < x.length; i++) {
-      document.getElementById("checkout-purchases").innerHTML +=
-        "<span class = 'details'>"+
-        x[i].getElementsByTagName("brand")[0].childNodes[0].nodeValue +
-        "</span> " +
-        x[i].getElementsByTagName("model")[0].childNodes[0].nodeValue +
-        "</span> " +
-        x[i].getElementsByTagName("size")[0].childNodes[0].nodeValue +
-        "</span> " +
-        x[i].getElementsByTagName("price")[0].childNodes[0].nodeValue +
-        "<br>";
-      }
-
-}
-
-loadPBasket("basket.xml");
 </script>
-
+<script src="checkout.js"></script>
 </body>
 </html>
