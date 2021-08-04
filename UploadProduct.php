bec228505d5d9cad50560d556e255964bba6a34e
Riley <riley.childs@yahoo.com>
2021-08-04 17:58:23 -0400

    8-4-21

--- UploadProduct.php
+++ UploadProduct.php
@@ -6,9 +6,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Upload Your Inventory</title>
 </head>
 <body>
+    <h1>Upload Your Product</h1>
     <form name="uploads"  method="post">
        <label>Brand:</label>
         <input type="text" name="brand"><br><br>
