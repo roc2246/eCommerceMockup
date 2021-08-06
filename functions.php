4bad77e8d051767b830bdf7aea3390d97639b8e5
Riley <riley.childs@yahoo.com>
2021-08-06 09:24:37 -0400

    Encryption

--- functions.php
+++ functions.php
@@ -132,6 +132,17 @@ function checkAvailable(){
 
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+ //Encrypts password
+ $username = mysqli_real_escape_string($connection, $username );   
+ $password = mysqli_real_escape_string($connection, $password );
+    
+ $hashFormat = "$2y$10$"; 
+ $salt = "iusesomecrazystrings22";
+ $hashF_and_salt = $hashFormat . $salt;
+ $password = crypt($password,$hashF_and_salt);   
+     
+
         
     $query = "SELECT * from users where username = '$username'";
 
