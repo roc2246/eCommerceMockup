e9b842972ec9dcce1b49564960d3d59f8ad4487e
Riley <riley.childs@yahoo.com>
2021-11-18 19:42:32 -0500

    Installed PHP and JavaScript linters

--- functions.php
+++ functions.php
@@ -1,58 +1,59 @@
 <?php
-
 //Register Username
-function checkAvailable($table, $loginPage, $col1, $col2){
+function checkAvailable($table, $loginPage, $col1, $col2)
+{
 
-  //Validates form data before submission
-  if (
-    empty($_POST[$col1]) && isset($_POST['submit']) ||
-    empty($_POST[$col2])&& isset($_POST['submit'])
-  ){
-    echo "<h4>Please enter a username and password!</h4>";
-  }else if(isset($_POST['submit']) && !empty($_POST)) {
-    global $connection;
+    /* Validates form data before submission */
+    if (empty($_POST[$col1]) && isset($_POST['submit']) 
+        || empty($_POST[$col2])&& isset($_POST['submit'])
+    ) {
+        echo "<h4>Please enter a username and password!</h4>";
+    } else if (isset($_POST['submit']) && !empty($_POST)) {
+        global $connection;
 
-  $username = $_POST[$col1];
-  $password = $_POST[$col2];     
-  $btwnQuotes = str_replace("'", " ", $col1);
+        $username = $_POST[$col1];
+        $password = $_POST[$col2];     
+        $btwnQuotes = str_replace("'", " ", $col1);
         
-  $query = "SELECT * from $table where $btwnQuotes = '$username'";
+        $query = "SELECT * from $table where $btwnQuotes = '$username'";
 
-  $result = mysqli_query($connection, $query);
-  $count = mysqli_num_rows($result);
+        $result = mysqli_query($connection, $query);
+        $count = mysqli_num_rows($result);
 
-  //Checks if username is available
-  if($count>0){
-    echo "User unavailable";
-  } else if($count == 0) {
+        //Checks if username is available
+        if ($count>0) {
+            echo "User unavailable";
+        } else if ($count == 0) {
 
-    //Encrypts password
-    $username = mysqli_real_escape_string($connection, $username );   
-    $password = mysqli_real_escape_string($connection, $password );
-    $hashFormat = "$2y$10$"; 
-    $salt = "iusesomecrazystrings22";
-    $hashF_and_salt = $hashFormat . $salt;
-    $password = crypt($password,$hashF_and_salt);   
+            //Encrypts password
+            $username = mysqli_real_escape_string($connection, $username );   
+            $password = mysqli_real_escape_string($connection, $password );
+            $hashFormat = "$2y$10$"; 
+            $salt = "iusesomecrazystrings22";
+            $hashF_and_salt = $hashFormat . $salt;
+            $password = crypt($password, $hashF_and_salt);   
 
-    //Creates New User
-    $query = "INSERT INTO $table($col1,$col2) ";
-    $query .= "VALUES ('$username', '$password')";  
-    header('Refresh: 2; URL = ' . $loginPage);
+            //Creates New User
+            $query = "INSERT INTO $table($col1,$col2) ";
+            $query .= "VALUES ('$username', '$password')";  
+            header('Refresh: 2; URL = ' . $loginPage);
    
-    //Checks if query is successful
-    $result = mysqli_query($connection, $query);
-    if(!$result) {
-      die("QUERY FAILED" . mysqli_error($connection));    
-    }else {
-      echo "User Created"; 
-    }
-  }
- } 
+            //Checks if query is successful
+            $result = mysqli_query($connection, $query);
+            if (!$result) {
+                die("QUERY FAILED" . mysqli_error($connection));    
+            } else {
+                echo "User Created"; 
+            }
+        }
+    } 
 }
 
 //Greets user/admin
-function greetUser(){
-  if (isset($_SESSION['username']) && isset($_SESSION['password'])){
+function greetUser()
+{
+    if (isset($_SESSION['username']) && isset($_SESSION['password']))
+    {
     echo "<h4>Hello, ".$_SESSION['username']."!  <a href='logoutCustomer.php'>Logout</a></h4>";
   }
 }
